ff1ef46202dfe94cc476d228163b6b9b69fb32f9
Lucas Schwarz <lucas.schwarz@outlook.com>
2017-07-19 13:31:10 -0700

    add a page with a range input

--- index.js
+++ index.js
@@ -221,6 +221,38 @@ app.get('/hover', function (req, res) {
 	`);
 });
 
+app.get('/range', function (req, res) {
+  var min = req.query.min || 1;
+  var max = req.query.max || 100;
+  var default_value = req.query.defaultValue || 50;
+  var input_id = 'range-input'
+  var output_id = 'range-output'
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <script>
+      function updateValue(val) {
+        input = document.getElementById("${input_id}");
+        output = document.getElementById("${output_id}");
+        output.innerText = val;
+        output.setAttribute("value", val);
+        input.setAttribute("value", val);
+      }
+  </script>
+  </head>
+  <body>
+    <h1 automation="halp">I am Jack's range input</h1>
+    <input type="range" name="range" id="${input_id}" min="${min}" max="${max}" defaultValue="${default_value}" value="${default_value}" onchange="updateValue(this.value)">
+    <br />
+    <output id="${output_id}" value="${default_value}">${default_value}</output>
+  </form>
+  </body>
+  </html>
+
+  `);
+});
+
 var server = app.listen(port, function () {
 	console.log(`Running! on http://localhost:%s`, port);
 	console.log(`Invoke like this http://localhost:%s/fields`, port)
@@ -230,4 +262,5 @@ var server = app.listen(port, function () {
 	console.log(`Invoke like this http://localhost:%s/cookies`, port)
   console.log(`Invoke like this http://localhost:%s/alert`, port)
   console.log(`Invoke like this http://localhost:%s/hover`, port)
+  console.log(`Invoke like this http://localhost:%s/range?min=100&max=1000&defaultValue=500`, port)
 });
